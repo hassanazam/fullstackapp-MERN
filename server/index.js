e0e8a8d6717a88c75ae0d439ef32c300394fb8fc
Hassan Azam <hassanazam@live.com>
2022-04-06 03:34:57 +0500

    installed passport js with google oauth. saved project credentials in keys config.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleAuthStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({'hello': 'world'})
-});
+passport.use(new GoogleAuthStrategy());
 
-const PORT = process.env.PORT || 5000
-app.listen(PORT); 
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
