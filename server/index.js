f128e455c4fbaf064556e1390b93a1440fe1b574
Hassan Azam <hassanazam@live.com>
2022-04-06 03:40:07 +0500

    added callback for google oath.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleAuthStrategy = require('passport-google-oauth20').Strategy;
-
+const keys = require('./config/keys')
 const app = express();
 
-passport.use(new GoogleAuthStrategy());
+passport.use(new GoogleAuthStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+        console.log(accessToken);
+    })
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
