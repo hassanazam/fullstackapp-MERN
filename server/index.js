0cd0bbe09ba16f4f6298050d1343246948db63bc
Hassan Azam <hassanazam@live.com>
2022-04-08 08:20:53 +0500

    added mongoose and atlas connectionstring.

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
-require('./services/passport')
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
 
+mongoose.connect(keys.mongoURI);
 const app = express();
+
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
