5c2167b1bd695a91d6c9471f68157dcb16ee6edb
Hassan Azam <hassanazam@live.com>
2022-04-08 08:37:03 +0500

    User model added with Google Profile ID obtained via oauth.

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,14 +1,15 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleAuthStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys')
+
+const User = mongoose.model('users');
 
 passport.use(new GoogleAuthStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
     }, (accessToken, refreshToken, profile, done) => {
-        console.log(accessToken);
-        console.log('Profile', profile);
-        console.log('Refresh token', refreshToken)
+        new User({ googleId: profile.id }).save();
     })
 );
