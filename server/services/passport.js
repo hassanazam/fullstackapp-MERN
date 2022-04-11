ec67f9a66810d9e29641b719a421393711ca7124
Hassan Azam <hassanazam@live.com>
2022-04-11 11:18:22 +0500

    refactored and used asyn/await syntax. added proxy for dev env.

--- server/services/passport.js
+++ server/services/passport.js
@@ -20,16 +20,14 @@ passport.use(new GoogleAuthStrategy({
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true,
-    }, (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id}).then(existingUser => {
-            if (existingUser) {
-                // user already exists
-                done(null, existingUser);
-            } 
-            else {
-                new User({ googleId: profile.id }).save().then(user => done(null, user));
-            }
-        });
+    }, async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id})
+        if (existingUser) {
+            return done(null, existingUser);
+        }
+
+        const user = await new User({ googleId: profile.id }).save()
+        return done(null, user);
         
     })
 );
