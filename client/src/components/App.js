d9b6c1ddf2f05025aeb28681ae4f9afca0951f72
Hassan Azam <hassanazam@live.com>
2022-04-13 09:39:50 +0500

    basic setup of react-router.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route} from 'react-router-dom';
+
+const Header = () => <h2>Heading2</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>SurveyNew</h2>
+const Landing = () => <h2>Landing</h2>
 
 const App = () => {
     return (
         <div>
-            Hi Hassan!
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Landing}></Route>
+                    <Route exact path="/surveys" component={Dashboard}></Route>
+                    <Route path="/surveys/new" component={SurveyNew}></Route>
+                </div>
+            </BrowserRouter>
         </div>
     );
 };
